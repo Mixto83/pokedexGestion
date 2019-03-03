46da7a0185fd2d3dffd5892389a268107ac739ac
juanam8 <juanam9k@gmail.com>
2019-03-04 00:43:54 +0100

    Interfaz wip

---
+++ js/index.js
@@ -0,0 +1,12 @@
+class Pokemon {
+    constructor(id, name, type1, type2, gen, legendary, nForms, image){
+        this.id = id;
+        this.name = name;
+        this.type1 = type1;
+        this.type2 = type2;
+        this.gen = gen;
+        this.legendary = legendary;
+        this.nForms = nForms;
+        this.image = image;
+    }
+}
